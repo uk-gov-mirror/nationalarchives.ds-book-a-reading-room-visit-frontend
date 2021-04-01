a7cab76e4cfcfd8b6e7ef1de4973bfcee8ebe27c
Gwyn Jones <m@gwnjns.com>
2021-04-01 14:04:45 +0100

    Introduce details and summary pattern for smaller screens

---
+++ book-a-reading-room-visit/js/select-a-date.js
@@ -0,0 +1,12 @@
+if (window.innerWidth <= 480) {
+
+    var details_elements = document.getElementsByTagName('details');
+
+    Array.prototype.forEach.call(details_elements, function (item) {
+        item.removeAttribute('open');
+    });
+}
+
+
+
+
