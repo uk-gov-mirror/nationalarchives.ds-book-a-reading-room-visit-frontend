cce5bcd2e5c16dcf7984b0252d5ee8d9f7baac0b
Gwyn Jones <m@gwnjns.com>
2021-04-01 11:17:17 +0100

    Add index page and make file names consistent with Axure

--- index.php
+++ index.php
@@ -1,4 +1,42 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kew booking system - frontend</title>
+</head>
+<body>
 
-include_once ('homepage.html');
+<ul>
+
+    <?php
+
+    function process_filename($str) {
+        $no_file_ext = str_replace('.html', '', $str);
+        $no_underscores = str_replace('_', ' ', $no_file_ext);
+        return ucfirst($no_underscores);
+    }
+
+    if ($handle = opendir('.')) {
+
+        while (false !== ($entry = readdir($handle))) {
+
+            if (strpos($entry, '.html') != false) {
+
+                $format = '<li><a href=\'/%s\'>%s</a></li>';
+                echo sprintf($format, $entry, process_filename($entry));
+            }
+        }
+
+        closedir($handle);
+    }
+
+    ?>
+
+</ul>
+
+</body>
+</html>
 
